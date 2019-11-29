0e8a118e2af72231c71726213cd1f7164dc672c2
luizsantana <lhzsantana@gmail.com>
2019-11-29 09:56:07 -0300

    Initial commit

--- src/main/java/com/local/kafka/AvroConsumer.java
+++ src/main/java/com/local/kafka/AvroConsumer.java
@@ -1,4 +1,4 @@
-package schema;
+package com.local.kafka;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
@@ -15,46 +15,41 @@ import java.util.stream.IntStream;
 public class AvroConsumer {
 
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
-    private final static String TOPIC = "new-employees";
+    private final static String TOPIC = "avro2";
 
-    private static Consumer<Long, Employee> createConsumer() {
+    private static Consumer<Long, Message> createConsumer() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleAvroConsumer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                LongDeserializer.class.getName());
-
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                KafkaAvroDeserializer.class.getName());
-
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
         props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
-        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
-                "http://localhost:8081");
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        //props.put(AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
 
         return new KafkaConsumer<>(props);
     }
 
     public static void main(String... args) {
 
-        final Consumer<Long, Employee> consumer = createConsumer();
+        final Consumer<Long, Message> consumer = createConsumer();
         consumer.subscribe(Collections.singletonList(TOPIC));
 
         IntStream.range(1, 100).forEach(index -> {
 
-            final ConsumerRecords<Long, Employee> records =
+            final ConsumerRecords<Long, Message> records =
                     consumer.poll(100);
+
+            consumer.subscribe(Collections.singletonList(TOPIC));
 
             if (records.count() == 0) {
                 System.out.println("None found");
             } else records.forEach(record -> {
 
-                Employee employeeRecord = record.value();
+                Message messageRecord = record.value();
 
-                System.out.printf("%s %d %d %s \n", record.topic(),
-                        record.partition(), record.offset(), employeeRecord);
+                System.out.printf("%s %d %d %s \n", record.topic(), record.partition(), record.offset(), messageRecord);
             });
         });
     }
-
-
 }
---
+++ src/main/java/com/local/kafka/AvroProducer.java
@@ -0,0 +1,46 @@
+package com.local.kafka;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+public class AvroProducer {
+
+    private final static String TOPIC = "avro2";
+
+    private static Producer<Long, Message> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "AvroProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+
+        //props.put(AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
+
+        return new KafkaProducer<>(props);
+    }
+
+    public static void main(String... args) {
+
+        Producer<Long, Message> producer = createProducer();
+
+        Message message = Message.newBuilder()
+                .setText("This is a text.")
+                .build();
+
+        IntStream.range(1, 100).forEach(index -> {
+            producer.send(new ProducerRecord<>(TOPIC, 1L * index, message));
+        });
+
+        producer.flush();
+        producer.close();
+    }
+}
